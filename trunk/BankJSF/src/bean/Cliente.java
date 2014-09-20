45412d9ea92e8daedf079edcf072545709c3ec44
renan.costa175@gmail.com <renan.costa175@gmail.com@d5557d0e-fe09-0035-83e5-852b2ae87e79>
2014-09-20 19:54:45 +0000

    Created Beans: Cliente ContaCorrente ContaPoupança

---
+++ trunk/BankJSF/src/bean/Cliente.java
@@ -0,0 +1,58 @@
+package bean;
+
+public class Cliente {
+	
+	private Integer id;
+	private String nome;
+	private Integer cpf;
+	private Integer agencia;
+	private String senha;
+	private ContaCorrente contac;
+	private ContaPoupanca contap;
+	
+	
+	public ContaCorrente getContac() {
+		return contac;
+	}
+	public void setContac(ContaCorrente contac) {
+		this.contac = contac;
+	}
+	public ContaPoupanca getContap() {
+		return contap;
+	}
+	public void setContap(ContaPoupanca contap) {
+		this.contap = contap;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getCpf() {
+		return cpf;
+	}
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+	public Integer getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(Integer agencia) {
+		this.agencia = agencia;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+
+}
---
+++ trunk/BankJSF/src/bean/ContaCorrente.java
@@ -0,0 +1,25 @@
+package bean;
+
+public class ContaCorrente {
+	
+	private Integer conta;
+	private Integer saldo;
+	
+	
+	public Integer getConta() {
+		return conta;
+	}
+	public void setConta(Integer conta) {
+		this.conta = conta;
+	}
+	public Integer getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Integer saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
+
+}
