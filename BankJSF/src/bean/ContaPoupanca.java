c3af080fa9a3f30d3421fe0fe5aa99225dadd433
renan.costa175@gmail.com <renan.costa175@gmail.com@d5557d0e-fe09-0035-83e5-852b2ae87e79>
2014-09-20 19:54:45 +0000

    Created Beans: Cliente ContaCorrente ContaPoupança

---
+++ BankJSF/src/bean/ContaPoupanca.java
@@ -0,0 +1,24 @@
+package bean;
+
+public class ContaPoupanca {
+	
+	private Integer conta;
+	private Integer saldo;
+	
+	
+	public Integer getConta() {
+		return conta;
+	}
+	public void setConta(Integer conta) {
+		this.conta = conta;
+	}
+	public Integer getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Integer saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+
+}
