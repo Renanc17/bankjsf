f36e8d19190db71079519f716f815ff4b266b20f
vincenzo.pagliari <vincenzo.pagliari@d5557d0e-fe09-0035-83e5-852b2ae87e79>
2014-10-19 09:34:54 +0000

    Não sei pq não lista os favoritos e não consigo debugar Cadastro de Favoritos Ok!

--- BankJSF/src/dao/FavoritoDAO.java
+++ BankJSF/src/dao/FavoritoDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import bean.Favorito;
 import factory.ConnectionFactory;
@@ -26,6 +28,37 @@ public class FavoritoDAO {
 		stmt.executeUpdate();
 		conn.close();
 		
+	}
+	
+	public List<Favorito> listarFavoritos(Integer idCliente) throws SQLException{
+		
+		Connection conn = ConnectionFactory.getConnection();
+		
+		String sql = "select * from favorito where idCliente=? order by nome";
+		
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		
+		stmt.setInt(1, idCliente);
+		
+		ResultSet rs = stmt.executeQuery();
+		
+		
+		ArrayList<Favorito> favoritos = new ArrayList<Favorito>();
+		
+		while(rs.next()){
+			Favorito f = new Favorito();
+			f.setId(rs.getInt("id"));
+			f.setNome(rs.getString("nome"));
+			f.setAgencia(rs.getInt("agencia"));
+			f.setContaC(rs.getInt("contaC"));
+			
+			favoritos.add(f);
+		}
+		
+		
+		conn.close();
+		
+		return favoritos;
 	}
 
 }
--- BankJSF/src/managedBean/FavoritoManagedBean.java
+++ BankJSF/src/managedBean/FavoritoManagedBean.java
@@ -1,6 +1,7 @@
 package managedBean;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -17,7 +18,9 @@ public class FavoritoManagedBean {
 	
 	private Favorito f = new Favorito();
 	private Cliente user = new Cliente();
+	private List<Favorito> listaFavoritos;
 	private String msg = "";
+	
 	
 	public Favorito getF() {
 		return f;
@@ -25,17 +28,23 @@ public class FavoritoManagedBean {
 	public void setF(Favorito f) {
 		this.f = f;
 	}
+	public Cliente getUser() {
+		return user;
+	}
+	public void setUser(Cliente user) {
+		this.user = user;
+	}
+	public List<Favorito> getListaFavoritos() {
+		return listaFavoritos;
+	}
+	public void setListaFavoritos(List<Favorito> listaFavoritos) {
+		this.listaFavoritos = listaFavoritos;
+	}
 	public String getMsg() {
 		return msg;
 	}
 	public void setMsg(String msg) {
 		this.msg = msg;
-	}
-	public Cliente getC() {
-		return user;
-	}
-	public void setC(Cliente user) {
-		this.user = user;
 	}
 	
 	public String cadastrarFavorito(){
@@ -55,6 +64,43 @@ public class FavoritoManagedBean {
 			msg = "Erro ao cadastrar o favorito";
 			pagina = "erro";
 		}	
+		
+		return pagina + ".faces?faces-redirect=true";
+	}
+	
+	public String listarFavoritos(){
+		FavoritoDAO dao = new FavoritoDAO();
+		String pagina = "Home";
+		
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+		user = (Cliente) session.getAttribute("cliente");
+		
+		try{
+			listaFavoritos = dao.listarFavoritos(user.getId());
+			pagina = "consultarFavoritos";
+		}catch(SQLException e){
+			pagina = "erro";
+		}
+
+		return pagina + ".faces?faces-redirect=true";
+	}
+	
+	public String alterarFavorito(){
+		
+		String pagina = "";
+		FavoritoDAO dao = new FavoritoDAO();
+		
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
+		user = (Cliente) session.getAttribute("cliente");
+		
+		try{
+			dao.cadastrarFavorito(f);
+			
+		}catch(SQLException e){
+			pagina="erro";
+		}
 		
 		return pagina + ".faces?faces-redirect=true";
 	}
